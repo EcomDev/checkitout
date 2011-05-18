d69114110ceea1103ec44cd7eeb13ad91465e0fc
ichepurnyi <ichepurnyi@9ae20c55-688b-4e83-9148-0752a5fcc63e>
2011-05-18 12:09:29 +0000

    ! JS fix for redirect payment method
    git-svn-id: http://svn.ecomdev.org/svn/checkitout/trunk@10 9ae20c55-688b-4e83-9148-0752a5fcc63e

--- skin/frontend/base/default/js/ecomdev/checkitout.js
+++ skin/frontend/base/default/js/ecomdev/checkitout.js
@@ -1524,8 +1524,7 @@ var Payment = Class.create(EcomDev.CheckItOut.Step, {
             }
         }
         
-        if ($('payment_form_'+method) && $('payment_form_'+method)
-        			.select('input', 'select', 'textarea').length > 0){
+        if ($('payment_form_'+method) && $('payment_form_'+method).select('input', 'select', 'textarea').length > 0){
             var form = $('payment_form_'+method);
             form.style.display = '';
             var elements = form.select('input', 'select', 'textarea');
