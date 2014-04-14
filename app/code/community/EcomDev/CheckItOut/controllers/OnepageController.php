479aa3e8c64b92453e7b22b85b9451f6716f17f5
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2014-04-14 11:56:02 +0200

    Fix 1.8CE compatibility

--- app/code/community/EcomDev/CheckItOut/controllers/OnepageController.php
+++ app/code/community/EcomDev/CheckItOut/controllers/OnepageController.php
@@ -567,6 +567,18 @@ class EcomDev_CheckItOut_OnepageController extends Mage_Checkout_OnepageControll
             try {
                 $this->getOnepage()->getQuote()
                     ->removeItem($quoteItem->getId());
+                
+                if ($this->getOnepage()->getQuote()->isVirtual()) {
+                    $address = $this->getOnepage()->getQuote()->getBillingAddress();
+                } else {
+                    $address = $this->getOnepage()->getQuote()->getShippingAddress();
+                }
+
+                // Remove cached properties for 1.8x
+                $address->unsetData('cached_items_all');
+                $address->unsetData('cached_items_nominal');
+                $address->unsetData('cached_items_nonnominal');
+                
                 $this->_recalculateTotals();
                 $result['success'] = true;
                 /**
