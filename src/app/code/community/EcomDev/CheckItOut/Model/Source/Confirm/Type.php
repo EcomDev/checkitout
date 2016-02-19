18af7b4f66e6e072951abcdb3785849f583ec7e0
Andrew Howden <hello@andrewhowden.com>
2016-02-19 20:00:30 +1100

    Ensure all relevant files have the OSL-3.0 license in the header
    This commit also includes a syntax error in src/shell/checkitout-geoip.php
    
    Tested (sanity checked) with:
        $ find . -name "*.php" -exec php -l {} \; > /dev/null
        $ find . -name "*.phtml" -exec php -l {} \; > /dev/null
        $ find . -name "*.xml" -exec xmllint {} \; > /dev/null
    
    Neither system tests nor checking within Magneto was run for this
    commit.

--- src/app/code/community/EcomDev/CheckItOut/Model/Source/Confirm/Type.php
+++ src/app/code/community/EcomDev/CheckItOut/Model/Source/Confirm/Type.php
@@ -7,12 +7,12 @@
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * https://opensource.org/licenses/osl-3.0.php
  *
  * @category   EcomDev
  * @package    EcomDev_CheckItOut
- * @copyright  Copyright (c) 2011 EcomDev BV (http://www.ecomdev.org)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright  Copyright (c) 2016 EcomDev BV (http://www.ecomdev.org)
+ * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @author     Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
  */
 
